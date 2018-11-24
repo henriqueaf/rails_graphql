2913169af968439229699511f1ee226961e646e7
Henrique Almeida <henrique.af1@gmail.com>
2018-11-24 11:59:11 -0300

    CreateUserMutation

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,3 +1,12 @@
+class Types::UserInputType < GraphQL::Schema::InputObject
+  graphql_name "UserInputType"
+  description "All the attributes for creating an User"
+
+  argument :id, ID, required: false
+  argument :first_name, String, required: false
+  argument :last_name, String, required: false
+end
+
 class Types::UserType < Types::BaseObject
   description "An User"
 
